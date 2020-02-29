fa1886adcead5c60edfc71f502bf4c28842e0855
grishinrv <roman1988grishin@gmail.com>
2020-02-29 10:18:11 +0300

    ToDoITem added and pluged

--- vue-app/src/components/ToDoList.vue
+++ vue-app/src/components/ToDoList.vue
@@ -1,7 +1,16 @@
 <template>
     <div>
         <ul>
-            <li>Item</li>
+            <ToDoItem/>
         </ul>
     </div>
 </template>
+
+<script>
+import ToDoItem from '@/components/ToDoItem'
+export default {
+    components:{
+        ToDoItem
+    }
+}
+</script>
